eab88102d35815ad253999f57b59a9791f3c18a2
NeithanT <n.vargas.5@estudiantec.cr>
2025-11-06 22:50:26 -0600

    Added Random Number Generator

---
+++ cmp.sh
@@ -0,0 +1,7 @@
+nasm -f elf32 $1.asm -o $1.o
+
+ld -s -m elf_i386 $1.o -o $1 io.o
+
+./$1
+
+rm $1.o $1
--- getRand.asm
+++ getRand.asm
@@ -4,37 +4,38 @@
 ;-------Curso IC-3101 Arquitectura de Computadoras----------
 ;--------------------Proyecto #01------------------------------
 ;---------Neithan Vargas Vargas, Carné: 2025149384----------
-;---2025/11/07 , II Periodo, Profesor: MS.c Esteban Arias---
+;---2025/11/12 , II Periodo, Profesor: MS.c Esteban Arias---
 
-%include "io.mac" ; Incluir macros para entrada y salida de datos
+%include "io.mac"
 
 .DATA
 
-    file_name   db "intro.txt", 0
-    
-.UDATA
-    buffer  resb 1000
+    file_name       db "intro.txt", 0
+    question_size   dd  30
+    eax_help        db "A VALUE:", 0
+    edx_help        db "D VALUE:", 0
 
 .CODE
 
     .STARTUP
-        ;                   eax     ebx
-        ;time	man/ cs/	0x0d	time_t *tloc
-
-        mov EAX, 13  ; syscall for time (returns seconds since epoch)
-        mov EBX, 0   ; NULL pointer
-        int 0x80
-
-        PutLInt EAX
-        nwln
-        
-        mov     ECX, 5
+    
+        ; Divide by amount of questions
+        ; To get the modulo after
+        rdtsc
+        xor EDX, EDX    ; only get the last part in EAX
+        mov ECX, [question_size] 
+        PutLInt ECX
         div ECX
 
-        PutInt CX
         nwln
-        PutInt AX
+        PutStr eax_help
         nwln
+        PutLInt EAX
+        nwln
+        PutStr edx_help
+        nwln
+        PutLInt EDX
+
     done:
         nwln    ; Imprime un salto de linea antes de finalizar
         .EXIT   ; Finaliza el programa
